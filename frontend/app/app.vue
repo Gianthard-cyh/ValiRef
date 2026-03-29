92273616e8566c069f5fe5d3226865bb5bf78700
0xfe <2953427626@qq.com>
2026-03-29 16:51:30 +0800

    feat: add Cloudflare Web Analytics tracking

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,3 +1,15 @@
 <template>
   <NuxtPage />
 </template>
+
+<script setup lang="ts">
+useHead({
+  script: [
+    {
+      defer: true,
+      src: 'https://static.cloudflareinsights.com/beacon.min.js',
+      'data-cf-beacon': '{"token": "849aa3ca00b64b6cab72ef455297fbc6"}'
+    }
+  ]
+})
+</script>
